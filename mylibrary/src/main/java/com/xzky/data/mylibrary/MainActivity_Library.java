2c619358e79313d1ec5b3fa32dda7e03bc4dc8ff
hanyy <1693293467@qq.com>
2018-03-28 14:35:20 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/xzky/data/mylibrary/MainActivity_Library.java
@@ -0,0 +1,16 @@
+package com.xzky.data.mylibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Vampire on 2018/3/23.
+ */
+
+public class MainActivity_Library extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
