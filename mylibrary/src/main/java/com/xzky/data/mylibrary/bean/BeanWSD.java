2104f1136b4b54bbe5f46693593e1e92857531f1
hanyy <1693293467@qq.com>
2018-03-29 09:34:23 +0800

    解析
    解析

---
+++ mylibrary/src/main/java/com/xzky/data/mylibrary/bean/BeanWSD.java
@@ -0,0 +1,28 @@
+package com.xzky.data.mylibrary.bean;
+
+/**
+ * Created by Vampire on 2018/3/28.
+ */
+
+public class BeanWSD {
+    /**
+     * 温度
+     */
+    public String wdStr;
+    /**
+     * 湿度
+     */
+    public String sdStr;
+    /**
+     * 大气压
+     */
+    public String dqyStr;
+    /**
+     * 信号
+     */
+    public int signal;
+    /**
+     * 电量
+     */
+    public int elec;
+}
